7c9407acdd98596ca75c5781bd7c6c90c0de61df
Koallla <zmiyov2422@gmail.com>
2019-12-04 19:04:50 +0200

    fixed validation register form

--- src/js/add-photo.js
+++ src/js/add-photo.js
@@ -14,7 +14,6 @@ import {
 verificationLocal();
 const isLogin = getLocal().isLogin;
 
-
 export default function() {
   if (!document.querySelector('#ADDPHOTO')) return;
 
--- src/js/registration-form.js
+++ src/js/registration-form.js
@@ -4,7 +4,7 @@ import PNotifyStyleMaterial from 'pnotify/dist/es/PNotifyStyleMaterial.js';
 import 'pnotify/dist/PNotifyBrightTheme.css';
 import getGeoPosition from './getGeoPosition';
 import { setLocal, getLocal, removeLocal } from './localStorage';
-export default function () {
+export default function() {
   if (!document.querySelector('#REGISTRATION')) return;
   //-----// position
   const onGetPositionSuccess = location => {
@@ -31,13 +31,53 @@ export default function () {
     fields: document.querySelectorAll('.field'),
   };
 
+  // Validation satrt
   let maleGroup = '';
+
+  refs.userLogin.onblur = function() {
+    refs.userLogin.value.length < 3
+      ? refs.userLogin.classList.add('empty') ||
+        PNotify.notice({
+          text: 'Name must contain at least 3 characters',
+        })
+      : refs.userLogin.classList.remove('empty');
+  };
+
+  refs.userTel.onblur = function() {
+    refs.userTel.value.length !== 10 || !refs.userTel.value.replace(/\D/g, '')
+      ? refs.userTel.classList.add('empty') ||
+        PNotify.notice({
+          text: 'Number must contain at least 10 symbols and only numbers',
+        })
+      : refs.userTel.classList.remove('empty');
+  };
+
+  refs.password.onblur = function() {
+    if (refs.password.value.length < 8) {
+      refs.password.classList.add('empty') ||
+        PNotify.notice({
+          text: 'Password must contain at least 8 symbols ',
+        });
+    } else if (
+      !refs.password.value.replace(/[^0-9]/g, '') ||
+      !refs.password.value.replace(/[^a-zA-Z]/g, '')
+    ) {
+      refs.password.classList.add('empty') ||
+        PNotify.notice({
+          text: 'Password must contain at least one number or letter',
+        });
+    } else {
+      refs.password.classList.remove('empty');
+    }
+  };
 
   refs.form.addEventListener('submit', e => {
     e.preventDefault();
     for (let i = 0; i < refs.fields.length; i++) {
       if (!refs.fields[i].value) {
         refs.fields[i].classList.add('empty');
+      } else if (refs.userLogin.value.length < 3) {
+        refs.userLogin.classList.add('empty');
       } else if (refs.password.value.length < 8) {
         refs.password.classList.add('empty');
       } else {
@@ -56,7 +96,7 @@ export default function () {
         geo_location: myGeoPosition,
         gender: maleGroup.value,
       })
-      .then(function (response) {
+      .then(function(response) {
         setLocal({
           isLogin: true,
           token: response.data.token,
@@ -67,9 +107,22 @@ export default function () {
           text: 'Registration success!',
         });
       })
-      .catch(function (error) {
+      .catch(function(error) {
         console.log(error);
       });
     refs.form.reset();
   });
 }
+
+/*
+
+ВАЛИДАЦИЯ: 
+Имя - не меньше трех символов с проверкой _throttle 500ms
+Телефон - кол-во символов 10, только цифры. (Проверка при потере фокуса)
+
+Возраст - только цифры!
+
+
+Пароль должен содержать цифры и буквы,
+кол-во символов > 8. (Подсказка о наличии цифр и букв в pnotify)
+*/
