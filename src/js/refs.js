06fc0c42d7c6c8143cd45f1bb708ec8610fcda78
YEvhen-SHmatko <yevhen.shmatko@gmail.com>
2019-12-12 21:46:57 +0200

    refresh

--- src/js/refs.js
+++
@@ -1,7 +0,0 @@
-export default {
-  map: document.getElementById('map'),
-  machPeopleUl: document.querySelector('.mach__people-ul'),
-  body: document.querySelector('body'),
-  exit: document.querySelector('body'),
-  menu: document.querySelector('.js-menu'),
-};
